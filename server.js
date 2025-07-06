4cd9ad6a923a2b0fcd00a0fbad52194486afd690
gusnovais <gustavonovaismpe@gmail.com>
2025-07-06 12:24:23 -0300

    Tirando o Limiter sem uso, pois foi substituído por generalLimiter

--- server.js
+++ server.js
@@ -53,13 +53,6 @@ const authLimiter = rateLimit({
   legacyHeaders: false,
 });
 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
-  message: 'Too many requests from this IP, please try again later.',
-});
-
-//app.use(limiter);
 app.use(generalLimiter);
 
 
